72371111adae7aae9990416c29def9da6513b4ec
Dedan <dedanirungu@gmail.com>
2024-04-12 10:00:50 +0300

    Add rights() method to Ipayug model

--- Entities/Ipayug.php
+++ Entities/Ipayug.php
@@ -75,4 +75,15 @@ class Ipayug extends BaseModel
 
         return $structure;
     }
+
+
+    /**
+     * Define rights for this model.
+     *
+     * @return array
+     */
+    public function rights(): array
+    {
+
+    }
 }
