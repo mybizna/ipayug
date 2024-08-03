b9721832012b71f2f163b36f9ac4d5db3bdc68b8
Dedan <dedanirungu@gmail.com>
2024-08-03 11:55:58 +0300

    Changes to files: Entities/Ipayug.php

--- Entities/Ipayug.php
+++ Entities/Ipayug.php
@@ -15,20 +15,6 @@ class Ipayug extends BaseModel
     protected $fillable = ['item_id', 'status', 'txncd', 'ivm', 'qwh', 'afd', 'poi', 'uyt', 'ifd', 'agd', 'mc', 'p1', 'p2', 'p3', 'p4', 'payment_id', 'is_processed'];
 
     /**
-     * The fields that are to be render when performing relationship queries.
-     *
-     * @var array<string>
-     */
-    public $rec_names = ['item_id', 'txncd'];
-
-    /**
-     * List of tables names that are need in this model during migration.
-     *
-     * @var array<string>
-     */
-    public array $migrationDependancy = [];
-
-    /**
      * The table associated with the model.
      *
      * @var string
@@ -65,31 +51,8 @@ class Ipayug extends BaseModel
         $this->fields->boolean('is_processed')->nullable()->html('switch');
     }
 
-    /**
-     * List of structure for this model.
-     */
-    public function structure($structure): array
-    {
-        $structure['table'] = ['item_id', 'status', 'txncd', 'ivm', 'qwh', 'afd', 'poi', 'uyt', 'ifd', 'agd', 'mc', 'p1', 'p2', 'p3', 'p4', 'payment_id', 'is_processed'];
-        $structure['filter'] = ['item_id', 'txncd'];
-
-        return $structure;
-    }
 
 
-    /**
-     * Define rights for this model.
-     *
-     * @return array
-     */
-    public function rights(): array
-    {
-        $rights = parent::rights();
 
-        $rights['staff'] = ['view' => true];
-        $rights['registered'] = ['view' => true];
-        $rights['guest'] = [];
 
-        return $rights;
-    }
 }
