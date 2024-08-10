dc9b75ab2611f1335dfd482688d176939619f3e6
Dedan <dedanirungu@gmail.com>
2024-08-10 23:06:43 +0300

    Changes to files: Classes/IpayugPaymentProcessor.php Entities/Ipayug.php

--- Classes/IpayugPaymentProcessor.php
+++ Classes/IpayugPaymentProcessor.php
@@ -6,9 +6,9 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
-use Modules\Ipay\Entities\IpayugPayment;
+use Modules\Ipay\Models\IpayugPayment;
 use Modules\Payment\Classes\PaymentProcessor;
-use Modules\Payment\Entities\Payment;
+use Modules\Payment\Models\Payment;
 
 class IpayugPaymentProcessor
 {
