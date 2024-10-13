dc8c8fb3a981f1a576d75ecaa9570a5b730dfa46
Dedan <dedanirungu@gmail.com>
2024-10-13 16:31:39 +0300

    Release 2024.13

---
+++ app/Filament/Resources/IpayugResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Ipayug\Filament\Resources\IpayugResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
---
+++ app/Filament/Resources/IpayugResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Ipayug\Filament\Resources\IpayugResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
