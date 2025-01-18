1a936d2391cad358030c78c936d4a2e957137a3c
Dedan <dedanirungu@gmail.com>
2025-01-18 14:23:15 +0300

    Changes to add database migration to model

--- app/Models/Ipayug.php
+++ app/Models/Ipayug.php
@@ -3,6 +3,7 @@
 namespace Modules\Ipayug\Models;
 
 use Modules\Base\Models\BaseModel;
+use Illuminate\Database\Schema\Blueprint;
 
 class Ipayug extends BaseModel
 {
@@ -20,4 +21,29 @@ class Ipayug extends BaseModel
      * @var string
      */
     protected $table = "ipayug_ipayug";
+
+    public function migration(Blueprint $table): void
+    {
+        $table->id();
+
+        $table->string('item_id')->nullable();
+        $table->string('status')->nullable();
+        $table->string('txncd')->nullable();
+        $table->string('ivm')->nullable();
+        $table->string('qwh')->nullable();
+        $table->string('afd')->nullable();
+        $table->string('poi')->nullable();
+        $table->string('uyt')->nullable();
+        $table->string('ifd')->nullable();
+        $table->string('agd')->nullable();
+        $table->string('mc')->nullable();
+        $table->string('p1')->nullable();
+        $table->string('p2')->nullable();
+        $table->string('p3')->nullable();
+        $table->string('p4')->nullable();
+        $table->foreignId('payment_id')->nullable()->constrained(table: 'account_payment')->onDelete('set null');
+        $table->boolean('is_processed')->nullable();
+
+
+    }
 }
